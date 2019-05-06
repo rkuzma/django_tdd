87354da169de81198fa5e4f94c03785b8c71b1f7
Radoslav Kuzma <radoslav.kuzma@t-systems.com>
2019-05-06 12:21:03 +0200

    Model for list Items and associated migration

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,9 @@
 
 # Create your views here.
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def home_page(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {
+        'new_item_text': request.POST.get('item_text', ''),
+    })
